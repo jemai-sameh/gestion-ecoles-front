f4d3466aa86ec3e8e28cd5e49150ddd1efc69d46
jemai-sameh <samehjemai98@gmail.com>
2023-04-06 21:46:21 +0100

    add seance and updated method

---
+++ src/app/services/seance/seance.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Seance } from 'src/app/models/seance';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeanceService {
+
+  baseurlSeance :string = environment.baseurl +"/seances"
+
+  constructor(private http: HttpClient) { }
+
+  saveSeance(seance: Seance): Observable<Seance> {
+    return this.http.post(`${this.baseurlSeance}/addOrUpdate`, seance)
+    .pipe(
+      map((response:any) => response as Seance)
+    );
+  }
+
+  /*getSeanceList(): Observable<Seance[]> {
+    return this.http.get<Seance[]>(`${this.baseurlSeance}/listeSeance`)
+    .pipe(
+      map((response:any) => response as Seance[])
+    );
+    
+  }*/
+  getSeanceList(id:number): Observable<Seance[]> {
+    return this.http.get<Seance[]>(`${this.baseurlSeance}/findAllBySchool/`+id)
+    .pipe(
+      map((response:any) => response as Seance[])
+    );
+    
+  }
+  deleteSeance(id: any): Observable<any> {
+    return this.http.delete(`${this.baseurlSeance}/deleteSeance/${id}`,{ responseType: 'text' });
+  }
+  findSeanceById(id: number): Observable<Seance> {
+    return this.http.get(`${this.baseurlSeance}/getbyid/${id}`).pipe(
+      map((response:any) => response as Seance)
+    );
+  }
+
+}
